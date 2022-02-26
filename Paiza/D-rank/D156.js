73a592ea5817a9af531a64acdc3f00aa9e9b204e
OhByeongHeon <hi1004@github.com>
2022-02-27 01:51:41 +0900

    add D-rank directory, D156:密度の計算

---
+++ Paiza/D-rank/D156.js
@@ -0,0 +1,7 @@
+const [N, M] = require('fs').readFileSync('./paiza/input.txt', 'utf8').split('\n').map(Number);
+
+function solution(N, M) {
+  const answer = N * M;
+  return answer;
+}
+console.log(solution(N, M));
