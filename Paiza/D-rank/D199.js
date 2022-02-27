22839a194abfd24c448b59853e3de77c1af3ed91
OhByeongHeon <hi1004@github.com>
2022-02-27 15:03:18 +0900

    D199:お餅の個数

---
+++ Paiza/D-rank/D199.js
@@ -0,0 +1,8 @@
+const [n, m] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(Number);
+
+function solution(n, m) {
+  const answer = n * m;
+  return answer;
+}
+
+console.log(solution(n, m));
