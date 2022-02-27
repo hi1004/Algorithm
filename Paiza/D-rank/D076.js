9dacf01296fd8c888e45e53c18e6f3b66ccf69c3
OhByeongHeon <hi1004@github.com>
2022-02-28 01:57:10 +0900

    D076:禁止ワード

--- Paiza/D-rank/D076.js
+++ Paiza/D-rank/D076.js
@@ -1,6 +1,7 @@
 const [W, S] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 
 function solution(W, S) {
+  // Array.includes()
   const answer = S.includes(W) ? 'NG' : S;
   return answer;
 }
