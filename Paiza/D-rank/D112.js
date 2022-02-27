81f6b45a3a6dcdf470df2f5b705b4aab304a9fb6
OhByeongHeon <hi1004@github.com>
2022-02-27 15:10:27 +0900

    D112:工場の生産力

---
+++ Paiza/D-rank/D112.js
@@ -0,0 +1,7 @@
+const [n, h] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(Number);
+
+function solution(n, h) {
+  const answer = n * h;
+  return answer;
+}
+console.log(solution(n, h));
