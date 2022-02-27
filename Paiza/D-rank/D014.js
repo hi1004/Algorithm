db97b0f950f4093580500a25c1db54d53eda9f13
OhByeongHeon <hi1004@github.com>
2022-02-27 23:29:35 +0900

    D014:小文字を大文字に

--- Paiza/D-rank/D014.js
+++ Paiza/D-rank/D014.js
@@ -1,6 +1,7 @@
 const [s] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split(' ');
 
 function solution(s) {
+  // String.toUpperCase()
   const answer = s.toUpperCase();
   return answer;
 }
