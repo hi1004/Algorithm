0810b7efd011325d128958ee4ef65c2938232a05
OhByeongHeon <hi1004@github.com>
2022-03-01 17:46:29 +0900

    D022:表面積の計算

--- Paiza/D-rank/D022.js
+++ Paiza/D-rank/D022.js
@@ -1,8 +1,15 @@
 const [a] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split(' ').map(Number);
 
-function solution() {
+function solution1() {
+  // Math.pow();
   const answer = 6 * Math.pow(a, 2);
   return answer;
 }
 
-console.log(solution());
+function solution2() {
+  const answer = 6 * 2 ** 2;
+  return answer;
+}
+
+console.log(solution1());
+console.log(solution2());
