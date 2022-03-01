aa53a581df9d5bff7c88b27dec9703d613d5fbf9
OhByeongHeon <hi1004@github.com>
2022-03-01 18:12:15 +0900

    D015:カウントダウン

--- Paiza/D-rank/D015.js
+++ Paiza/D-rank/D015.js
@@ -1,5 +1,17 @@
 const [n] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split(' ').map(Number);
 
-for (let i = n; i > 0; i--) {
-  console.log(i);
+function solution1() {
+  for (let i = n; i > 0; i--) {
+    console.log(i);
+  }
 }
+
+function solution2() {
+  while (n > 0) {
+    console.log(n);
+    n--;
+  }
+}
+
+solution1();
+solution2();
