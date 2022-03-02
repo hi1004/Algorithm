eea13650871ce222c8a9e08c110d74f505285219
OhByeongHeon <hi1004@github.com>
2022-03-02 14:56:32 +0900

    D188:2枚のカード

--- Paiza/D-rank/D188.js
+++ Paiza/D-rank/D188.js
@@ -1,4 +1,4 @@
-const [a, b] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n').map(Number);
+const [a, b] = require('fs').readFileSync('/dev/stdin', 'utf8').trim().split('\n');
 
 function solution() {
   const answer = a + b === 21 ? 'Win' : a + b;
