b79184bc57b4965e308b8542deb1584f449e12c1
OhByeongHeon <hi1004@github.com>
2022-03-14 15:22:29 +0900

    D046:不思議なタマゴ

---
+++ Paiza/D-rank/D046.js
@@ -0,0 +1,6 @@
+const [d_1, d_2, d_3] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+(function solution() {
+  const answer = Math.max(d_1, d_2, d_3);
+  console.log(first);
+})();
