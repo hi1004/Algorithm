e3c7754b10c5eb9bd305b2839a1d3175f781d536
OhByeongHeon <hi1004@github.com>
2022-03-15 13:46:50 +0900

    D084:英語で何月？

---
+++ Paiza/D-rank/D084.js
@@ -0,0 +1,19 @@
+const [M] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
+
+(function solution() {
+  const months = {
+    1: 'January',
+    2: 'February',
+    3: 'March',
+    4: 'April',
+    5: 'May',
+    6: 'June',
+    7: 'July',
+    8: 'August',
+    9: 'September',
+    10: 'October',
+    11: 'November',
+    12: 'December',
+  };
+  console.log(months[M]);
+});
