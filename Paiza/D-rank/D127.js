d705d3ea1cb82c4d56164ce602186b0f949f7c76
OhByeongHeon <hi1004@github.com>
2022-03-17 13:16:48 +0900

    D127:座席番号のくじ

--- Paiza/D-rank/D127.js
+++ Paiza/D-rank/D127.js
@@ -1,6 +1,6 @@
 const [S] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 
-(function solution() {
+(function solution1() {
   let answer = '';
   S.split('').forEach(s => {
     if (S[1] === S[2] && S[1] === S[3]) {
@@ -9,5 +9,10 @@ const [S] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' '
       answer = 'No';
     }
   });
+  console.log(answer);
+})();
+
+(function solution2() {
+  const answer = S[1] === S[2] && S[1] === S[3] ? 'Yes' : 'No';
   console.log(answer);
 })();
