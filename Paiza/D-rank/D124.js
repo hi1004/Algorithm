d478f7b4f850ae26136cbbef50296497fc099675
OhByeongHeon <hi1004@github.com>
2022-03-18 19:01:17 +0900

    D124:あと何日

--- Paiza/D-rank/D124.js
+++ Paiza/D-rank/D124.js
@@ -1,6 +1,7 @@
 const [T] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(Number);
 
 (function solution() {
+  // Math.ceil();
   const answer = T <= 24 ? 1 : Math.ceil(T / 24);
   console.log(answer);
 })();
