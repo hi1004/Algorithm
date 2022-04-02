3cf4856a7d50c2b033d426030570e32257e13201
OhByeongHeon <hi1004@github.com>
2022-04-02 15:42:35 +0900

    D113:初日の出

---
+++ Paiza/D-rank/D113.js
@@ -0,0 +1,16 @@
+let [h, m] = require('fs')
+  .readFileSync('/dev/stdin')
+  .toString()
+  .trim()
+  .split(':')
+  .map(Number);
+
+(function solution() {
+  const minute = 0 <= m && m < 60 ? m : '00';
+  const hour = h < 0 || h >= 24 ? '00' : h;
+  const timeHour =
+    Number(hour - 8) < 0 ? 24 + Number(hour - 8) : Number(hour - 8);
+  console.log(hour, minute);
+  const answer = `${timeHour}:${Number(minute)}`;
+  console.log(answer);
+})();
