3b016550fd10b61eacd3aaf0186050a8fd1b825d
OhByeongHeon <hi1004@github.com>
2022-04-09 03:04:35 +0900

    숫자의 개수

--- Backjoon/2577.js
+++ Backjoon/2577.js
@@ -8,6 +8,6 @@ let multiple = input[0] * input[1] * input[2];
 const arr = multiple.toString().split('');
 let result = new Array(10).fill(0);
 arr.forEach(x => {
-  result[x] = (result[x] || 0) + 1;
+  result[x] += 1;
 });
 console.log(result.join('\n'));
