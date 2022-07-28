8ae791a87f61092068400c7a67346e3ed3854f9e
OhByeongHeon <hi1004@github.com>
2022-07-28 23:19:43 +0900

    피보나치 수

---
+++ Backjoon/t.js
@@ -0,0 +1,16 @@
+const [n] = require('fs')
+  .readFileSync('./input.txt', 'utf-8')
+  .split(' ')
+  .map(Number);
+
+function fib(num) {
+  let temp = [0, 1];
+
+  if (num === 0) return 0;
+  for (let i = 0; i < num - 1; i++) {
+    temp.push(temp[i] + temp[i + 1]);
+  }
+  return temp.pop();
+}
+
+console.log(fib(n));
