3d35a703ee7c41c1121f749c90c11a5fde33df68
OhByeongHeon <hi1004@github.com>
2022-07-28 23:11:02 +0900

    피보나치 수

--- Backjoon/10870.js
+++ Backjoon/10870.js
@@ -1,4 +1,5 @@
 const n = require('fs').readFileSync('./input.txt', 'utf-8');
+
 function fib(num) {
   if (num <= 0) return 0;
   if (num <= 2) return 1;
---
+++ Backjoon/2747.js
@@ -0,0 +1,17 @@
+const [n] = require('fs')
+  .readFileSync('./input.txt', 'utf-8')
+  .split(' ')
+  .map(Number);
+
+function fib(num) {
+  let temp = [0, 1];
+
+  if (num === 0) return 0;
+  for (let i = 0; i < num - 1; i++) {
+    temp.push(temp[i] + temp[i + 1]);
+  }
+
+  return temp[temp.length - 1];
+}
+
+console.log(fib(n));
