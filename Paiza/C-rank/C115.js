aa385922b506d3c0bc3843c4899b518fd3b6af0e
OhByeongHeon <hi1004@github.com>
2022-08-09 16:28:03 +0900

    渋滞の距離

---
+++ Paiza/C-rank/C115.js
@@ -0,0 +1,14 @@
+const Main = input => {
+  const [N, M] = input[0].split(' ').map(Number);
+
+  const cars = [];
+  for (let i = 1; i < N; i++) {
+    cars.push(Number(input[i]));
+  }
+  const result = cars
+    .filter(car => car <= M)
+    .reduce((acc, cur) => acc + cur, 0);
+  console.log(result);
+};
+
+Main(require('fs').readFileSync('./input.txt', 'utf-8').trim().split('\n'));
