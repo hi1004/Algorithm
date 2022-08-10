9b638edf1b818d21763e95b1de18e75409573392
OhByeongHeon <hi1004@github.com>
2022-08-10 22:06:24 +0900

    ログのフィルター

--- Paiza/C-rank/C055.js
+++ Paiza/C-rank/C055.js
@@ -1,20 +1,16 @@
 const Main = input => {
-  const n = Number(input[0]);
   const target = input[1].toString().trim();
-
   const lists = input.slice(2).map(list => list.trim());
-
-  const result = lists.filter(list => {
-    if (list.includes(target)) {
-      return list;
+  let flag = false;
+  for (let ans of lists) {
+    if (ans.includes(target)) {
+      flag = true;
+      console.log(ans);
     }
-  });
-  if (result.length === 0) {
+  }
+  if (!flag) {
     console.log('None');
   }
-  result.forEach(ans => {
-    console.log(ans);
-  });
 };
 
 Main(require('fs').readFileSync('./input.txt', 'utf-8').trim().split('\n'));
