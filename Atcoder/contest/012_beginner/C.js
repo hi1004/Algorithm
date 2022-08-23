5c0db1b871c21e54fb819ac2e195d88b43c92aea
OhByeongHeon <hi1004@github.com>
2022-08-23 13:23:48 +0900

    九九足し算

--- Atcoder/contest/012_beginner/C.js
+++ Atcoder/contest/012_beginner/C.js
@@ -1,24 +1,13 @@
 const Main = input => {
   const n = Number(input);
-  let sum = 0;
+  let sum = 2025 - n;
   for (let i = 1; i <= 9; i++) {
     for (let j = 1; j <= 9; j++) {
-      sum += i * j;
+      if (sum === i * j) {
+        console.log(`${i} x ${j}`);
+      }
     }
   }
-
-  const arr = [];
-  for (let i = 1; i <= 9; i++) {
-    if ((sum - n) % i === 0) {
-      arr.push(i);
-    }
-  }
-
-  arr.forEach(v => {
-    if ((sum - n) / v <= 9) {
-      console.log(v + ' x ' + (sum - n) / v);
-    }
-  });
 };
 
 Main(require('fs').readFileSync('./input.txt', 'utf-8').trim());
