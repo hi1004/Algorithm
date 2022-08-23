96f3c9caedf202d518a917406a6c55103bc1d907
OhByeongHeon <hi1004@github.com>
2022-08-23 14:54:51 +0900

    錠

--- Atcoder/contest/013_beginner/B.js
+++ Atcoder/contest/013_beginner/B.js
@@ -1,12 +1,7 @@
 const Main = input => {
   let [a, b] = input.map(v => Number(v));
-  const big = 10 - Math.max(a, b);
 
-  if (Math.abs(a - b > 4)) {
-    console.log(big + Math.min(a, b));
-  } else {
-    console.log(Math.min(Math.abs(a - b), big + Math.min(a, b)));
-  }
+  console.log(Math.min(Math.abs(a - b), 10 - Math.abs(a - b)));
 };
 
 Main(require('fs').readFileSync('./input.txt', 'utf-8').trim().split('\n'));
