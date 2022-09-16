66e029a1c2ef0ced3eaf14aa0e54a689a0dbea34
OhByeongHeon <hi1004@github.com>
2022-09-17 00:02:47 +0900

    ポイントカードの計算

--- Paiza/C-rank/C015.js
+++ Paiza/C-rank/C015.js
@@ -4,19 +4,20 @@ const Main = line => {
   const data = line.slice(1).map(val => val.split(' ').map(Number));
   let discount = 0.01;
   let point = 0;
-  console.log(data);
-  for (let i = 0; i < data.length; i++) {
-    for (let j = 0; j < data[i].length; j++) {
-      if (data[i][0].toString().split('').includes('3')) {
+
+  data.forEach(arr => {
+    for (let j = 0; j < arr.length; j++) {
+      const day = arr[0].toString().split('');
+      if (day.includes('3')) {
         discount = 0.03;
-      } else if (data[i][0].toString().split('').includes('5')) {
+      } else if (day.includes('5')) {
         discount = 0.05;
       } else {
         discount = 0.01;
       }
     }
-    point += Math.floor(data[i][1] * discount);
-  }
+    point += Math.floor(arr[1] * discount);
+  });
   console.log(point);
 };
 
