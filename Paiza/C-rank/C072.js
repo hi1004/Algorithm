4b4d51a568a68e26dc24359d4ef19b6786298adb
OhByeongHeon <hi1004@github.com>
2022-09-21 00:16:19 +0900

    モンスターの進化

---
+++ Paiza/C-rank/C072.js
@@ -0,0 +1,42 @@
+const Main = input => {
+  const [atk, def, agi] = input[0].split(' ').map(Number);
+  const n = input[1];
+  const table = input.slice(2).map(data => data.trim().split(' '));
+
+  const info = {};
+
+  function solution(arr) {
+    // const levelUp = (userArr) => {
+    //   if (info[name][j] <= atk && atk <= info[name][j + 1]) {
+    //     console.log('atk');
+    //     isLevelUp = true;
+    //     break;
+    //   }
+    // }
+    let isLevelUp = false;
+    let userList = [];
+    for (let i = 0; i < n; i++) {
+      const name = arr[i][0];
+      const data = arr[i].slice(1).map(Number);
+
+      info[name] = data;
+
+      if (
+        info[name][0] <= atk &&
+        atk <= info[name][1] &&
+        info[name][2] <= def &&
+        def <= info[name][3] &&
+        info[name][4] <= agi &&
+        agi <= info[name][5]
+      ) {
+        isLevelUp = true;
+        userList.push(name);
+      }
+    }
+    return isLevelUp ? userList.join('\n') : 'no evolution';
+  }
+
+  console.log(solution(table));
+};
+
+Main(require('fs').readFileSync('./input.txt', 'utf8').split('\n'));
