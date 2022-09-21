99fc35863f08ec099ebab771f9bfc9c90cff1053
OhByeongHeon <hi1004@github.com>
2022-09-21 16:54:26 +0900

    그룹 단어 체커

---
+++ Backjoon/1316.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+const arr = [];
+
+for (let i = 1; i < input.length; i++) {
+  arr.push(input[i]);
+}
+
+let answer = arr.length;
+let temp = '';
+
+arr.forEach(item => {
+  temp = item[0];
+  for (let i = 1; i < item.length; i++) {
+    if (temp.includes(item[i]) && item[i - 1] !== item[i]) {
+      answer--;
+      break;
+    }
+    temp += item[i];
+  }
+});
+
+console.log(answer);
