b02422f0af4e107de1142a419486a3e69da2234d
OhByeongHeon <hi1004@github.com>
2022-09-21 22:10:09 +0900

    A+B - 2

---
+++ Backjoon/2558.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [A, B] = fs.readFileSync(file).toString().trim().split('\n').map(Number);
+
+console.log(A + B);
