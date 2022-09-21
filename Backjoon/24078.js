99476a96344e30699ac79d557d4b7767ef1fa25e
OhByeongHeon <hi1004@github.com>
2022-09-22 00:57:32 +0900

    余り (Remainder)

---
+++ Backjoon/24078.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const numbers = fs.readFileSync(file).toString().trim();
+
+console.log(Number(numbers) % 21);
