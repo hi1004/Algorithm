6be019e0ec2fc17f6ca2b2577af5df885e6bb33f
ByeongHeon, Oh <byorusia@gmail.com>
2022-09-22 18:49:12 +0900

    [Silver V] Title: 단어 정렬, Time: 5652 ms, Memory: 22388 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const line = fs.readFileSync(file).toString().trim().split('\n');
+const n = Number(line.shift());
+const words = line.map(word => word.trim());
+words.sort();
+
+const arr = words.filter((item, index) => {
+  return words.indexOf(item) === index;
+});
+
+const collection = {};
+
+for (let val of arr) {
+  collection[val] = val;
+}
+
+console.log(arr.sort((a, b) => a.length - b.length).join('\n'));
