c7c8f67c13c1c0d6cca14293414d8106f5c7723c
ByeongHeon, Oh <byorusia@gmail.com>
2022-09-23 13:03:57 +0900

    [Bronze II] Title: 블랙잭, Time: 196 ms, Memory: 15280 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -1,17 +1,20 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const line = fs.readFileSync(file).toString().trim().split('\n');
-const [n, m] = line[0].split(' ').map(Number);
-const cardNumbers = line[1].split(' ').map(Number);
+const input = fs.readFileSync(file).toString().trim().split('\n');
 
+const [n, m] = input[0].split(' ').map(Number);
+const numbers = input[1].split(' ').map(Number);
+const result = [];
 let sum = 0;
-let arr = [];
 for (let i = 0; i < n; i++) {
   for (let j = i + 1; j < n; j++) {
     for (let k = j + 1; k < n; k++) {
-      sum = cardNumbers[i] + cardNumbers[j] + cardNumbers[k];
-      arr.push(sum);
+      sum = numbers[i] + numbers[j] + numbers[k];
+      if (sum <= m) {
+        result.push(sum);
+      }
     }
   }
 }
-console.log(Math.max(...arr.filter(card => card <= m)));
+
+console.log(Math.max(...result));
