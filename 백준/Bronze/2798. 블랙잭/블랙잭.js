8bbc420ab50f3f499a8b0ddd3732246142846ae3
ByeongHeon, Oh <byorusia@gmail.com>
2022-09-23 13:06:58 +0900

    [Bronze II] Title: 블랙잭, Time: 188 ms, Memory: 15268 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.js
+++ 백준/Bronze/2798. 블랙잭/블랙잭.js
@@ -6,12 +6,15 @@ const [n, m] = input[0].split(' ').map(Number);
 const numbers = input[1].split(' ').map(Number);
 const result = [];
 let sum = 0;
+
 for (let i = 0; i < n; i++) {
   for (let j = i + 1; j < n; j++) {
     for (let k = j + 1; k < n; k++) {
       sum = numbers[i] + numbers[j] + numbers[k];
       if (sum <= m) {
         result.push(sum);
+      } else {
+        continue;
       }
     }
   }
