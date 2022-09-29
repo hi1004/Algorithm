b06dcc7b25b3e3398f67bb908c886117c65f52fe
ByeongHeon, Oh <byorusia@gmail.com>
2022-09-29 14:07:26 +0900

    [Bronze IV] Title: アイスクリーム (Ice Cream), Time: 124 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/24087. アイスクリーム （Ice Cream）/アイスクリーム （Ice Cream）.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [s, a, b] = fs
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+
+const ans = a <= s ? 250 + 100 * Math.ceil((s - a) / b) : 250;
+console.log(ans);
