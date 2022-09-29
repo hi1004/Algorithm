e0c55385ea62979f29c7df67a8fe90f6e534242d
OhByeongHeon <hi1004@github.com>
2022-09-30 04:27:03 +0900

    高橋くんの研修

---
+++ Atcoder/contest/015_beginner/A.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+
+const [a, b] = input;
+
+console.log(a.length > b.length ? a : b);
