014440c6cf45601a04646edc0823f47c1a33a715
ByeongHeon, Oh <byorusia@gmail.com>
2022-09-29 13:42:05 +0900

    [Bronze IV] Title: 試験 (Exam), Time: 148 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/18411. 試験 （Exam）/試験 （Exam）.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split(' ').map(Number);
+
+function solution(arr) {
+  if (!arr.length) return null;
+  arr.sort((a, b) => b - a);
+
+  return arr[0] + arr[1];
+}
+
+console.log(solution(input));
