668502aaaf2f3dae83d8611b3ef40707dd90a89c
ByeongHeon, Oh <byorusia@gmail.com>
2022-09-30 20:36:09 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 128 ms, Memory: 9552 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [n] = fs.readFileSync(file).toString().trim().split(' ').map(Number);
+
+for (let i = 1; i <= 2 * n - 1; i += 2) {
+  console.log(' '.repeat((2 * n - 1 - i) / 2) + '*'.repeat(i));
+}
