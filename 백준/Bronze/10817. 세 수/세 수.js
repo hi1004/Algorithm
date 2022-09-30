dbce13716af713965cb0506aecd3c91cca3cdd4c
ByeongHeon, Oh <byorusia@gmail.com>
2022-09-30 19:24:07 +0900

    [Bronze III] Title: 세 수, Time: 156 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10817. 세 수/세 수.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split(' ').map(Number);
+input.sort((a,b) => a-b)
+
+console.log(input[1])
