1390b3bb541f8a2db29a03ae2faeecfa7c71b0d8
ByeongHeon, Oh <byorusia@gmail.com>
2022-10-03 15:14:57 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 128 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [x, y] = fs.readFileSync(file).toString().trim().split(' ').map(Number);
+
+console.log(Math.min(Math.floor(x / 2), y));
