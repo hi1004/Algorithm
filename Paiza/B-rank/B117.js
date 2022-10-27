32ec45961db04304a1923d84cff30819f7b13dba
OhByeongHeon <hi1004@github.com>
2022-10-28 00:18:15 +0900

    回る教習車

---
+++ Paiza/B-rank/B117.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [N, ...nums] = fs
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split('\n')
+  .map(Number);
+
+nums.reverse();
+
+let count = 1;
+let result = 0;
+
+while (nums.length !== 1) {
+  const pop = nums.pop();
+  if (N === pop) {
+    result++;
+  }
+  nums.unshift(pop);
+
+  if (pop === count) {
+    count++;
+    nums.shift();
+  }
+}
+console.log(result);
