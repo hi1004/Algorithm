f0c6736468a1f743aae0819fc084960836efea76
OhByeongHeon <hi1004@github.com>
2022-11-03 12:03:43 +0900

    繰り返す

---
+++ Paiza/D-rank/D224.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [input] = fs.readFileSync(file).toString().trim().split(' ');
+
+console.log(input.repeat(3));
