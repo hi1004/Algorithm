3d5d844875e1dc33074caf4280147f889c369542
ByeongHeon, Oh <byorusia@gmail.com>
2022-12-17 18:08:35 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 112 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split('\n');
+
+console.log(input.length);
