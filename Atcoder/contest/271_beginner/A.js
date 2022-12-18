deceb53f0c2396d7a99c99c439d3628eee67956c
hi1004 <kipo09@naver.com>
2022-12-19 05:12:31 +0900

    484558

--- Atcoder/contest/271_beginner/A.js
+++ Atcoder/contest/271_beginner/A.js
@@ -1,14 +1,6 @@
 const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(file).toString().trim().split('\n');
+const [N] = fs.readFileSync(file).toString().trim().split(' ').map(Number);
 
-const [N, Q] = input.shift().split(' ').map(Number);
-const quarys = input.slice(0, N).map(nums => nums.split(' ').map(Number));
-
-const arrs = input.slice(N).map(nums => nums.split(' ').map(Number));
-
-for (let i = 0; i < Q; i++) {
-  const [index, point] = arrs[i];
-
-  console.log(quarys[index - 1][point]);
-}
+const answer = N.toString(16).toUpperCase();
+console.log(answer.length !== 2 ? '0' + answer : answer);
