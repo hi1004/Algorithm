0846abeeb47a166e55132d8e999312bdfd2951de
hi1004 <kipo09@naver.com>
2023-02-03 16:54:33 +0900

    The First Problem

---
+++ Atcoder/競技プログラミング鉄則/A01.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [n] = fs.readFileSync(file).toString().trim().split(' ');
+console.log(n * n);
