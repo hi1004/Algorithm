e3be5600d7e63a01de7be301aeff1005181b85e2
hi1004 <kipo09@naver.com>
2023-02-04 11:38:51 +0900

    Binary Representation 1

---
+++ Atcoder/競技プログラミング鉄則/A04.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [N] = fs.readFileSync(file).toString().trim().split(' ').map(Number);
+
+console.log(N.toString(2).padStart(10, '0'));
