be31743aa7f331e5e442763a237279d43688b995
hi1004 <kipo09@naver.com>
2023-02-17 01:08:04 +0900

    AtCoDeerくんとペンキ

---
+++ Atcoder/contest/046_beginner/A.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(file).toString().trim().split(/\s+/).map(Number);
+console.log(new Set(input).size);
