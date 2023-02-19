c6240a97fe1b1e1212a1927e8ace26dccde67192
hi1004 <kipo09@naver.com>
2023-02-20 00:46:24 +0900

    たてなが

---
+++ Atcoder/contest/049_beginner/B.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [w, h, ...input] = fs.readFileSync(file).toString().trim().split(/\s+/);
+console.log(w, h, input);
+
+for (let i = 0; i < w * 2; i++) {
+  console.log(input[Math.floor(i / 2)]);
+}
