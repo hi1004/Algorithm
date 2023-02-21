cd8f1d57cc100fe86929c729434ab5f7169d38bc
hi1004 <kipo09@naver.com>
2023-02-22 00:13:53 +0900

    Haiku

---
+++ Atcoder/contest/051_beginner/A.js
@@ -0,0 +1,4 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [input] = fs.readFileSync(file).toString().trim().split(/\s+/);
+console.log(input.split(',').join(' '));
