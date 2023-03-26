c4a22df30ce89f4ecbd63ebc5b745dcf2bdf86af
ByeongHeon, Oh <byorusia@gmail.com>
2023-03-26 18:08:33 +0900

    [Bronze V] Title: 직사각형, Time: 116 ms, Memory: 9592 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [a, b] = fs
+  .readFileSync(file)
+  .toString()
+  .trim()
+  .split(/\s+/)
+  .map(s => (isNaN(s) ? s : +s));
+
+console.log(a * b);
