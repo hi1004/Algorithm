b43a7fc95d2b2605645671cdd1025ccb8df27fd2
hi1004 <kipo09@naver.com>
2023-03-27 17:20:12 +0900

    New Year

---
+++ Atcoder/contest/084_beginner/A.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const [a] = fs.readFileSync(file).toString().trim().split(/\s+/).map(Number);
+
+console.log(24 - a + 24);
