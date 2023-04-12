7fda78fce89c1ceea870bac316aa98d8e8607407
hi1004 <kipo09@naver.com>
2023-04-12 15:27:12 +0900

    所持金の合計

---
+++ Paiza/D-rank/D242.js
@@ -0,0 +1,11 @@
+let [[a], [b]] = require('fs')
+  .readFileSync('./input.txt', 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+console.log(a + b);
