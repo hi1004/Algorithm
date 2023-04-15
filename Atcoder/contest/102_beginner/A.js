5dda74fb80af0ac38de26c974950b4644d2851aa
hi1004 <kipo09@naver.com>
2023-04-15 18:17:23 +0900

    Multiple of 2 and N

---
+++ Atcoder/contest/102_beginner/A.js
@@ -0,0 +1,13 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[n]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+
+console.log(n % 2 !== 0 ? n * 2 : n);
