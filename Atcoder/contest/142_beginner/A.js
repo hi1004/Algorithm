d8fec6ddfd9878025f0d121950c5a1bcdf342c10
hi1004 <kipo09@naver.com>
2023-05-29 14:46:54 +0900

    Odds of Oddness

---
+++ Atcoder/contest/142_beginner/A.js
@@ -0,0 +1,13 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[n]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+const nums = Array.from({ length: n }, (_, i) => i + 1);
+console.log(nums.filter(v => v % 2).length / n);
