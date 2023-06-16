861f186ff8a25335f349148d3039a790989debc7
hi1004 <kipo09@naver.com>
2023-06-16 11:43:46 +0900

    Coffee

---
+++ Atcoder/contest/160_beginner/A.js
@@ -0,0 +1,13 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[s]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+
+console.log(s[2] === s[3] && s[4] === s[5] ? 'Yes' : 'No');
