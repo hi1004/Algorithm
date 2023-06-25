2a840e272fab56165866772ad137c77ac1f6601a
hi1004 <kipo09@naver.com>
2023-06-25 18:50:35 +0900

    Multiplication 1

--- Atcoder/contest/169_beginner/A.js
+++ Atcoder/contest/169_beginner/A.js
@@ -1,22 +1,13 @@
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 let [[n], [...a]] = require('fs')
-    .readFileSync(file, 'utf8')
-    .trim()
-    .split('\n')
-    .map(r =>
-      r
-        .trim()
-        .split(/\s+/)
-        .map(i => (isNaN(i) ? i : BigInt(i)))
-    ),
-  m = 1n;
-if (a.indexOf(0n) == -1) {
-  for (c of a) {
-    m *= c;
-    if (BigInt(10 ** 18) < m) {
-      m = -1;
-      break;
-    }
-  }
-  console.log(String(m));
-} else console.log(0);
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : BigInt(i)))
+  );
+const result = a.reduce((num1, num2) => BigInt(num1) * BigInt(num2), 1);
+console.log(result > Math.pow(10, 18) ? -1 : result.toString());
