91c197dc22e817ee5b7bdd71f478fd6955ce8757
hi1004 <kipo09@naver.com>
2023-06-25 18:49:16 +0900

    Multiplication 2

--- Atcoder/contest/169_beginner/A.js
+++ Atcoder/contest/169_beginner/A.js
@@ -1,12 +1,22 @@
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-let [[a, b]] = require('fs')
-  .readFileSync(file, 'utf8')
-  .trim()
-  .split('\n')
-  .map(r =>
-    r
-      .trim()
-      .split(/\s+/)
-      .map(i => (isNaN(i) ? i : i - 0))
-  );
-console.log(a * b);
+let [[n], [...a]] = require('fs')
+    .readFileSync(file, 'utf8')
+    .trim()
+    .split('\n')
+    .map(r =>
+      r
+        .trim()
+        .split(/\s+/)
+        .map(i => (isNaN(i) ? i : BigInt(i)))
+    ),
+  m = 1n;
+if (a.indexOf(0n) == -1) {
+  for (c of a) {
+    m *= c;
+    if (BigInt(10 ** 18) < m) {
+      m = -1;
+      break;
+    }
+  }
+  console.log(String(m));
+} else console.log(0);
