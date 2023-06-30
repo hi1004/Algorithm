28d6208251507dc21970e4153538160445733162
hi1004 <kipo09@naver.com>
2023-07-01 00:39:25 +0900

    Rainy Season

---
+++ Atcoder/contest/175_beginner/A.js
@@ -0,0 +1,15 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[s]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+
+const answer = s.match(/R+/g);
+console.log(answer);
+console.log(answer ? answer[0].length : 0);
