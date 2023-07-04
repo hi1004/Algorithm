bf46e6a4f7885751248d4206c962c83f5931139e
hi1004 <kipo09@naver.com>
2023-07-04 13:24:32 +0900

    Not

---
+++ Atcoder/contest/178_beginner/A.js
@@ -0,0 +1,12 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[x]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+console.log(x ? 0 : 1);
