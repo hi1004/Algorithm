7c2514f5a8fee7790ba7c44d97d980c655046f4c
hi1004 <kipo09@naver.com>
2023-07-05 14:07:29 +0900

    Plural Form

---
+++ Atcoder/contest/179_beginner/A.js
@@ -0,0 +1,12 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[s]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+console.log(s[s.length - 1] === 's' ? `${s}es` : `${s}s`);
