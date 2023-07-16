7a04b780136ccd66380d3b916cde15183ec4cc36
hi1004 <kipo09@naver.com>
2023-07-16 18:59:58 +0900

    Slot

---
+++ Atcoder/contest/189_beginner/A.js
@@ -0,0 +1,12 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[s]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+console.log(new Set(s.split('')).size === 1 ? 'Won' : 'Lost');
