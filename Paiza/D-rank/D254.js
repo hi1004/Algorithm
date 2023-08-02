c3e26a0f2bd55eb7dd5575d9449d2285e38aabe0
hi1004 <kipo09@naver.com>
2023-08-02 23:12:30 +0900

    【2023年813の日】砲丸投げ

---
+++ Paiza/D-rank/D254.js
@@ -0,0 +1,13 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[a], [b]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+
+console.log(a - b);
