b002ca568f4382eccaaba7c81cecf5e3b2c422bc
hi1004 <kipo09@naver.com>
2023-08-04 10:59:27 +0900

    Maxi-Buying

---
+++ Atcoder/contest/206_beginner/A.js
@@ -0,0 +1,14 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[n]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+console.log(
+  n * 1.08 < 206 ? 'Yay!' : Math.floor(n * 1.08) === 206 ? 'so-so' : ':('
+);
