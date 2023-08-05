a95c36abcd7e93d3072e173de6e02b48c1ec07b1
hi1004 <kipo09@naver.com>
2023-08-05 23:34:27 +0900

    Repression

---
+++ Atcoder/contest/207_beginner/A.js
@@ -0,0 +1,13 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[...a]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+a = a.sort((x, y) => y - x);
+console.log(a[0] + a[1]);
