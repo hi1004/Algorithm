c5efd4a9c94a98a607c215c0d3f002cef61a746f
hi1004 <kipo09@naver.com>
2023-08-13 11:39:18 +0900

    Booby Prize

---
+++ Atcoder/contest/213_beginner/B.js
@@ -0,0 +1,14 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[n], [...a]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i - 0))
+  );
+
+const sorted = [...a].sort((a, b) => a - b);
+console.log(a.indexOf(sorted[n - 2]) + 1);
