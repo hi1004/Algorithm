95bfa96dd4c659c71b77effce43b398181c6daa1
hi1004 <kipo09@naver.com>
2023-08-28 14:56:06 +0900

    後ろ文字D

---
+++ Paiza/D-rank/D257.js
@@ -0,0 +1,13 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[s]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : +i))
+  );
+
+console.log(s[s.length - 1] === 'd' ? 'Yes' : 'No');
