9977bd78fde6361838bb285c78c8f969bbc7d69e
hi1004 <kipo09@naver.com>
2023-09-01 17:39:11 +0900

    QQ solver

---
+++ Atcoder/contest/232_beginner/A.js
@@ -0,0 +1,13 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[s]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : +i))
+  );
+const [a, b] = s.split('x');
+console.log(a * b);
