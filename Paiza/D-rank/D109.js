405fb3df31d2e368f2ea7e29f12b5309b3a86277
hi1004 <kipo09@naver.com>
2023-09-03 20:12:58 +0900

    D109

---
+++ Paiza/D-rank/D109.js
@@ -0,0 +1,12 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[m, d]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : i))
+  );
+console.log(new Set(m + d).size === 1 ? 'Yes' : 'No');
