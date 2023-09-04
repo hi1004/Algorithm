e0f3355786f7c53d5f2367d399bf3fbcd2ad218f
hi1004 <kipo09@naver.com>
2023-09-04 18:09:23 +0900

    カード並べ

--- Paiza/C-rank/C051.js
+++ Paiza/C-rank/C051.js
@@ -1,38 +1,13 @@
-const fs = require('fs');
 const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
-const input = fs.readFileSync(file).toString().trim().split(' ');
-input.sort((a, b) => b - a);
-
-const collection = {};
-
-let arrs = [];
-for (let i = 0; i < input.length; i++) {
-  for (let j = 1 + i; j < input.length; j++) {
-    arrs.push(input[i] + input[j]);
-  }
-}
-const reverse = arrs.map(n => n.split('').reverse().join(''));
-
-let concatArr = arrs.concat(reverse);
-
-let newArr = [...input];
-
-let temp = [];
-let result = [];
-const answer = [];
-concatArr.forEach(num => {
-  const [a, b] = num.split('');
-  newArr = [...input];
-  temp = [];
-  result = [];
-  temp.push(...num.split(''));
-
-  newArr.splice(newArr.indexOf(a), 1);
-
-  newArr.splice(newArr.indexOf(b), 1);
-
-  result.push(newArr.flat());
-
-  answer.push(Number(temp.join('')) + Number(result.flat().join('')));
-});
-console.log(Math.max(...answer));
+let [[...a]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : +i))
+  );
+a = a.sort((a, b) => b - a).map(v => String(v));
+console.log(Number(a[0] + a[2]) + Number(a[1] + a[3]));
