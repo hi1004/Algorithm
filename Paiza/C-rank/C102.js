fdd7d2db2b21f0dc67d30899574993f197c893c9
hi1004 <kipo09@naver.com>
2023-09-08 21:38:58 +0900

    :行きたいライブのスケジュール

--- Paiza/C-rank/C102.js
+++ Paiza/C-rank/C102.js
@@ -13,16 +13,15 @@ let [...days] = require('fs')
 
 const aDays = days.slice(1, days[0] + 1);
 const bDays = days.slice(days[0] + 2);
-let ans = null,
+let ans = [],
   isRive = false;
 
 for (let i = 1; i <= 31; i++) {
   if (aDays.includes(i) && bDays.includes(i)) {
-    !isRive ? (ans = 'A') : (ans = 'B');
+    !isRive ? ans.push('A') : ans.push('B');
     isRive = !isRive;
-  } else if (aDays.includes(i)) ans = 'A';
-  else if (bDays.includes(i)) ans = 'B';
-  else ans = 'x';
-
-  console.log(ans);
+  } else if (aDays.includes(i)) ans.push('A');
+  else if (bDays.includes(i)) ans.push('B');
+  else ans.push('x');
 }
+console.log(ans.join('\n'));
