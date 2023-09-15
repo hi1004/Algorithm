6b74a481e3465877f405a32491be661dc35cc01e
hi1004 <kipo09@naver.com>
2023-09-16 03:51:16 +0900

    Mex

---
+++ Atcoder/contest/245_beginner/B.js
@@ -0,0 +1,15 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[n], [...a]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : +i))
+  );
+
+a = new Set(a);
+const ans = [...a].sort((a, b) => a - b).filter((v, i) => v === i);
+console.log(ans.length);
