83514e8a5df3d44bdb25dbeed6ae92fbe36c5c02
hi1004 <kipo09@naver.com>
2023-09-20 08:27:21 +0900

    Lacked Number

---
+++ Atcoder/contest/248_beginner/A.js
@@ -0,0 +1,16 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [[s]] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : +i))
+  );
+
+const ans = Array.from({ length: 10 }, (_, i) => i)
+  .filter(v => ![...s.toString()].map(x => +x).includes(v))
+  .join('');
+console.log(ans);
