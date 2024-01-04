6ae358e7584205a5bc3c4ca34db461900855bad5
hi1004 <kipo09@naver.com>
2024-01-04 21:10:00 +0900

    再来月

---
+++ Paiza/D-rank/D273.js
@@ -0,0 +1,14 @@
+const file = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+let [n] = require('fs')
+  .readFileSync(file, 'utf8')
+  .trim()
+  .split('\n')
+  .map(r =>
+    r
+      .trim()
+      .split(/\s+/)
+      .map(i => (isNaN(i) ? i : +i))
+  )
+  .flat();
+
+console.log((n + 2) % 12 === 0 ? 12 : (n + 2) % 12);
